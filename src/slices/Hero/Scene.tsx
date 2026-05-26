6622a459b4e9e4c39039a499739239808887d2d2
3GGYlol <xlj135@gmail.com>
2026-05-26 18:42:16 -0400

    Added Keycaps

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Keyboard } from "@/components/Keyboard";
+import { Keycap } from "@/components/Keycap";
 import { Environment } from "@react-three/drei";
 import { PerspectiveCamera } from "@react-three/drei/core/PerspectiveCamera";
 import { useControls } from "leva";
@@ -17,14 +18,34 @@ export function Scene() {
         rotationZ: 0
     });
 
+    const scalingFactor = window.innerWidth <= 500 ? .5 : 1;
+    
     return (
         <group>
 
             <PerspectiveCamera makeDefault position={[0, 0, 5]} fov={80} />
+
+
+            <group scale={scalingFactor}> 
             <Keyboard 
             scale={9} 
             position={[0.2, -0.5, 3.8]}
-             rotation={[1.6, 0.4, 0]} />
+             rotation={[1.6, 0.4, 0]} /> 
+            
+            <group>
+                <Keycap position={[0, -0.4, 4.2]} rotation={[0, 2, 3]} texture={0} />
+                <Keycap position={[-1.4, 0, 4.2]} rotation={[3, 2, 1]} texture={1} />
+                <Keycap position={[-1.8, 1, 4.2]} rotation={[0, 1, 3]} texture={2} />
+                <Keycap position={[0, 1, 4.2]} rotation={[0, 4, 2]} texture={3} />
+                <Keycap position={[0.7, 0.9, 4.2]} rotation={[3, 2, 0]} texture={4} />
+                <Keycap position={[1.3, -0.3, 4.2]} rotation={[1, 2, 0]} texture={5} />
+                <Keycap position={[0, 1, 4.2]} rotation={[2, 2, 3]} texture={6} />
+                <Keycap position={[-0.7, 0.6, 4.2]} rotation={[1, 4, 0]} texture={7} />
+                <Keycap position={[-0.77, 0.1, 4.2]} rotation={[3, 2, 3]} texture={8} />
+                <Keycap position={[2, 0, 4.2]} rotation={[0, 0, 3]} texture={7} />
+            </group>
+            </group>
+            
             <Environment files={["/hdr/blue-studio.hdr"]}
             environmentIntensity={0.1} />
 
