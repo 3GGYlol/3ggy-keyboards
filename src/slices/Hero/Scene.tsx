3d622cd7a1cd17aed62bbbbde5f300ef6218f197
3GGYlol <xlj135@gmail.com>
2026-05-27 17:00:47 -0400

    keycaapss

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -33,15 +33,15 @@ export function Scene() {
              rotation={[1.6, 0.4, 0]} /> 
             
             <group>
-                <Keycap position={[0, -0.4, 4.2]} rotation={[0, 2, 3]} texture={0} />
+                <Keycap position={[0, -0.4, 3.2]} rotation={[0, 2, 3]} texture={0} />
                 <Keycap position={[-1.4, 0, 4.2]} rotation={[3, 2, 1]} texture={1} />
-                <Keycap position={[-1.8, 1, 4.2]} rotation={[0, 1, 3]} texture={2} />
+                <Keycap position={[-1.8, 1, 3.4]} rotation={[0, 1, 3]} texture={2} />
                 <Keycap position={[0, 1, 4.2]} rotation={[0, 4, 2]} texture={3} />
                 <Keycap position={[0.7, 0.9, 4.2]} rotation={[3, 2, 0]} texture={4} />
-                <Keycap position={[1.3, -0.3, 4.2]} rotation={[1, 2, 0]} texture={5} />
+                <Keycap position={[1.3, -0.3, 3.4]} rotation={[1, 2, 0]} texture={5} />
                 <Keycap position={[0, 1, 4.2]} rotation={[2, 2, 3]} texture={6} />
-                <Keycap position={[-0.7, 0.6, 4.2]} rotation={[1, 4, 0]} texture={7} />
-                <Keycap position={[-0.77, 0.1, 4.2]} rotation={[3, 2, 3]} texture={8} />
+                <Keycap position={[-0.7, 0.6, 3.0]} rotation={[1, 4, 0]} texture={7} />
+                <Keycap position={[-0.77, 0.1, 4.5]} rotation={[3, 2, 3]} texture={8} />
                 <Keycap position={[2, 0, 4.2]} rotation={[0, 0, 3]} texture={7} />
             </group>
             </group>
