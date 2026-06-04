bac351f730b30c186ff62d74ce95dcdb46811f72
3GGYlol <xlj135@gmail.com>
2026-06-04 12:21:40 -0400

    bentobox added

--- src/slices/BentoBox/index.tsx
+++ src/slices/BentoBox/index.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import { asText, Content } from "@prismicio/client";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
 import { Bounded } from "@/components/Bounded";
 import { PrismicNextImage } from "@prismicio/next";
 import clsx from "clsx";
+
+
 
 /**
  * Props for `BentoBox`.
@@ -19,16 +21,19 @@ const BentoBox: FC<BentoBoxProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <h2 className="font-bold-slanted mb-8 scroll-pt6 text-6xl uppercase
-      md:text-8xl">
-        <PrismicRichText field={slice.primary.heading} />
-      </h2>
       
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-6">
-      {slice.primary.items.map((item) => (
-        <BentoBoxItem key={asText(item.text)} item={item} />
-))}
+        <h2
+          id="features"
+          className="font-bold-slanted mb-8 scroll-pt-6 text-6xl uppercase md:text-8xl"
+        >
+          <PrismicText field={slice.primary.heading} />
+        </h2>
+     
 
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-6">
+        {slice.primary.items.map((item) => (
+          <BentoBoxItem key={asText(item.text)} item={item} />
+        ))}
       </div>
     </Bounded>
   );
@@ -38,31 +43,30 @@ export default BentoBox;
 
 type BentoBoxItemProps = {
   item: Content.BentoBoxSliceDefaultPrimaryItemsItem;
-}
+};
 
-
-function BentoBoxItem({item}: BentoBoxItemProps) {
-  
+function BentoBoxItem({ item }: BentoBoxItemProps) {
   return (
-  <div className={clsx(
-    "relative overflow-hidden rounded-3xl",
-      item.size === "Small" && "md:col-span-2",
-      item.size === "Medium" && "md:col-span-3",
-      item.size === "Large" && "md:col-span-4",
-    )}>
-          <PrismicNextImage field={item.image} 
-          className="h-full w-full object-cover" 
-          quality={100}
-          width={700}
-          />
-          <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-b
-          from-transparent to-black"> 
-          </div> 
-          
-          <div className="absolute bottom-0 left-0 max-w-xl p-6 text-xl text-balance text-white">
-          <PrismicRichText field={item.text} />
-        </div>
+    <div
+      className={clsx(
+        "relative overflow-hidden rounded-3xl",
+        item.size === "Small" && "md:col-span-2",
+        item.size === "Medium" && "md:col-span-3",
+        item.size === "Large" && "md:col-span-4",
+      )}
+    >
+      <PrismicNextImage
+        field={item.image}
+        className="h-full w-full object-cover"
+        quality={96}
+        width={700}
+      />
 
-        </div>
+      <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-b from-transparent to-black"></div>
+
+      <div className="absolute bottom-0 left-0 max-w-xl p-6 text-xl text-balance text-white">
+        <PrismicRichText field={item.text} />
+      </div>
+    </div>
   );
-} 
+}
