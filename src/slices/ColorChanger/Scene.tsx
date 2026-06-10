6d991962d85d427c581c73ce0756eaadae0707eb
3GGYlol <xlj135@gmail.com>
2026-06-10 18:47:53 -0400

    colorchanger added

--- src/slices/ColorChanger/Scene.tsx
+++ src/slices/ColorChanger/Scene.tsx
@@ -1,5 +1,8 @@
 import { Keyboard } from "@/components/Keyboard";
-
+import { Stage, useTexture } from "@react-three/drei";
+import { KEYCAP_TEXTURES } from ".";
+import { useMemo } from "react";
+import * as THREE from "three";
 
 type SceneProps = {
     selectedTextureId: string;
@@ -9,11 +12,41 @@ type SceneProps = {
 export function Scene({
      selectedTextureId, 
      
-     onAnimationComplete 
+     onAnimationComplete, 
     }: SceneProps) {
+
+     const texturePaths = KEYCAP_TEXTURES.map((t) =>t.path )
+     const textures = useTexture(texturePaths);
+     
+     const materials = useMemo(() => {
+        const materialMap: { [key:string]: THREE.MeshStandardMaterial} = {};
+        KEYCAP_TEXTURES.forEach((textureConfig, index) => {
+            const texture = Array.isArray(textures) ? textures[index] : textures
+
+            if (texture) {
+                texture.flipY = false
+                texture.colorSpace = THREE.SRGBColorSpace;
+
+                materialMap[textureConfig.id] = new THREE.MeshStandardMaterial({
+                    map: texture,
+                    roughness:0.7,
+                });
+        }
+     });
+
+        return materialMap;
+     }, [textures]);
+
+        const currentKnobColor = KEYCAP_TEXTURES.find((t) => t.id === selectedTextureId)?.knobColor || "#E24818";
+
        return(
+        <Stage environment={"city"} intensity={0.022} shadows={"contact"}>
+            
         <group>
-            <Keyboard />
+            <Keyboard 
+            keycapMaterial={materials[selectedTextureId]}
+            knobColor={currentKnobColor}/>
         </group>
+        </Stage>
     )
 }
--- src/slices/ColorChanger/index.tsx
+++ src/slices/ColorChanger/index.tsx
@@ -2,13 +2,16 @@
 
 import { FC, useCallback, useState } from "react";
 import { Content } from "@prismicio/client";
-import { PrismicText, SliceComponentProps } from "@prismicio/react";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
 import { Bounded } from "@/components/Bounded";
-import Image from "next/image";
 import clsx from "clsx";
+import Image from "next/image";
 import { Canvas } from "@react-three/fiber";
 import { Scene } from "./Scene";
-
 
 export const KEYCAP_TEXTURES = [
   {
@@ -29,10 +32,7 @@ export const KEYCAP_TEXTURES = [
     path: "/cherrynavy_uv.png",
     knobColor: "#F06B7E",
   },
-  { id: "kick", 
-    name: "Kick", 
-    path: "/kick_uv.png", 
-    knobColor: "#FD0A0A" },
+  { id: "kick", name: "Kick", path: "/kick_uv.png", knobColor: "#FD0A0A" },
   {
     id: "oldschool",
     name: "Old School",
@@ -48,6 +48,7 @@ export const KEYCAP_TEXTURES = [
 ];
 
 type KeycapTexture = (typeof KEYCAP_TEXTURES)[number];
+
 /**
  * Props for `ColorChanger`.
  */
@@ -57,18 +58,23 @@ export type ColorChangerProps = SliceComponentProps<Content.ColorChangerSlice>;
  * Component for "ColorChanger" Slices.
  */
 const ColorChanger: FC<ColorChangerProps> = ({ slice }) => {
-  const [selectedTextureId, setSelectedTextureId] = useState(KEYCAP_TEXTURES[0].id);
-  const [backgroundText, setBackgroundText] = useState(KEYCAP_TEXTURES[0].name);
+  const [selectedTextureId, setSelectedTextureId] = useState(
+    KEYCAP_TEXTURES[0].id,
+  );
+  const [backgroundText, setBackroundText] = useState(KEYCAP_TEXTURES[0].name);
   const [isAnimating, setIsAnimating] = useState(false);
-  
-  
-  function handleTextureSelect(texture: KeycapTexture){
-   if (texture.id === selectedTextureId || isAnimating) return; // Prevent re-selecting the same texture or selecting while animating
 
-   setSelectedTextureId(texture.id);
+  function handleTextureSelect(texture: KeycapTexture) {
+    if (texture.id === selectedTextureId || isAnimating) return;
+
+    //setIsAnimating(true);
+    setSelectedTextureId(texture.id);
+    setBackroundText(
+      KEYCAP_TEXTURES.find((t) => t.id === texture.id)?.name || "",
+    );
   }
-  
-  const handleAnimationComplete = useCallback(()=> {
+
+  const handleAnimationComplete = useCallback(() => {
     setIsAnimating(false);
   }, []);
 
@@ -76,52 +82,77 @@ const ColorChanger: FC<ColorChangerProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="relative flex h-[90vh] min-h-[1000px] flex-col overflow-hidden
-      bg-linear-to-br from-[#0f172a] to-[#062f4a] text-white"
+      className="relative flex h-[90vh] min-h-[1000px] flex-col overflow-hidden bg-linear-to-br from-[#0f172a] to-[#062f4a] text-white"
+      id="keycap-changer"
     >
       {/* SVG background */}
-      {/*Canvas} */}
-      <Canvas>
-        <Scene selectedTextureId={selectedTextureId} onAnimationComplete=
-        {handleAnimationComplete}/>
+      <svg
+        className="pointer-events-none absolute top-0 left-0 h-auto w-full mix-blend-overlay"
+        viewBox="0 0 75 100"
+      >
+        <text
+          fontSize={7}
+          textAnchor="middle"
+          dominantBaseline={"middle"}
+          x="50%"
+          y="50%"
+          className="font-black-slanted fill-white/20 uppercase group-hover:fill-white/30 motion-safe:transition-all motion-safe:duration-700"
+        >
+          {Array.from({ length: 20 }, (_, i) => (
+            <tspan key={i} x={`${(i + 1) * 10}%`} dy={i === 0 ? -50 : 6}>
+              {Array.from({ length: 10 }, () => backgroundText).join(" ")}
+            </tspan>
+          ))}
+        </text>
+      </svg>
+      {/* Canvas */}
+      <Canvas
+        camera={{ position: [0, 0.5, 0.5], fov: 45, zoom: 1.5 }}
+        className="-mb-[10vh] grow"
+      >
+        <Scene
+          selectedTextureId={selectedTextureId}
+          onAnimationComplete={handleAnimationComplete}
+        />
       </Canvas>
-      <Bounded className="relative shrink-0" innerClassName="gap-6 lg:gap-8 flex
-      flex-col lg:flex-row">
+      <Bounded
+        className="relative shrink-0"
+        innerClassName="gap-6 lg:gap-8 flex flex-col lg:flex-row"
+      >
         <div className="max-w-md shrink-0">
-          <h2 className="font-bold-slanted mb-1 text-4xl uppercase lg:md-2 lg:text-6xl">
+          <h2 className="font-bold-slanted mb-1 text-4xl uppercase lg:mb-2 lg:text-6xl">
             <PrismicText field={slice.primary.heading} />
           </h2>
           <div className="text-pretty lg:text-lg">
-            <PrismicText field={slice.primary.description} />
+            <PrismicRichText field={slice.primary.description} />
           </div>
         </div>
-        <ul className="grid grow grid-cols-2 gap-3 rounded-2xl bg-white p-4
-        text-black shadow-lg sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-3
-        xl:grid-cols-6"> 
+        <ul className="grid grow grid-cols-2 gap-3 rounded-2xl bg-white p-4 text-black shadow-lg sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-3 xl:grid-cols-6">
           {KEYCAP_TEXTURES.map((texture) => (
             <li key={texture.id}>
-              <button 
-              onClick={() => handleTextureSelect(texture)}
-              className={clsx(
-                "flex aspect-square flex-col items-center justify-center rounded-lg border-2 p-4 hover:scale-105 motion-safe:transition-all motion-safe:duration-300",
-                selectedTextureId === texture.id ? "border-[#81BFED] bg-[#81BFED]/20" : "cursor-pointer  border-gray-300 hover:border-gray-500",
-                isAnimating && "cursor-not-allowed opacity-50",
+              <button
+                onClick={() => handleTextureSelect(texture)}
+                disabled={isAnimating}
+                className={clsx(
+                  "flex aspect-square flex-col items-center justify-center rounded-lg border-2 p-4 hover:scale-105 motion-safe:transition-all motion-safe:duration-300",
+                  selectedTextureId === texture.id
+                    ? "border-[#81BFED] bg-[#81BFED]/20"
+                    : "cursor-pointer border-gray-300 hover:border-gray-500",
+                  isAnimating && "cursor-not-allowed opacity-50",
                 )}
-                >
-                {/* Handle selected texture ^ */}
-                <div className="mb-3 overflow-hidden rounded border-2 border-black
-                bg-gray-100">
-                  <Image 
-                  src={texture.path} 
-                  alt={texture.name} 
-                  width={100} 
-                  height={100} 
-                  className="h-full w-full object-cover" 
-                  /> 
-                  </div>
-                  <span className="text-center text-sm font-semibold">
-                    {texture.name}
-                  </span>
+              >
+                <div className="mb-3 overflow-hidden rounded border-2 border-black bg-gray-100">
+                  <Image
+                    src={texture.path}
+                    alt={texture.name}
+                    width={400}
+                    height={255}
+                    className="h-full w-full object-cover"
+                  />
+                </div>
+                <span className="text-center text-sm font-semibold">
+                  {texture.name}
+                </span>
               </button>
             </li>
           ))}
