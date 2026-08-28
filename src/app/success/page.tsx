43430803e6b86c9f7c52f21d24468375f229809c
3GGYlol <xlj135@gmail.com>
2026-08-28 15:02:03 -0400

    fix

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -14,7 +14,7 @@ import { Logo } from "@/components/Logo";
 import { FadeIn } from "@/components/FadeIn";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-06-24.dahlia",
+  
 });
 
 export const metadata: Metadata = {
